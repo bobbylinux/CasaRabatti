35420c665b9356745134d8c5b821a0b19f3ad592
bobbylinux <roberto.bani@gmail.com>
2016-02-18 17:03:23 +0100

    aggiunto tema one-page, aggiunto master slider per slide in home page

--- wp-content/themes/casarabatti/index.php
+++ wp-content/themes/casarabatti/index.php
@@ -1,7 +1,82 @@
 <?php get_header(); ?>
-    <div class="row">
-        <div class="col-xs-12">
-            <!--slider -->
+
+<!-- Intro Header -->
+<header class="intro">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <h1 class="brand-heading">Lorem ipsum</h1>
+                    <p class="intro-text">Duis facilisis eros et purus iaculis feugiat. Praesent enim mauris, eleifend non mauris molestie, tincidunt dignissim.</p>
+                    <a href="#struttura" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
-<?php get_footer(); ?>
+</header>
+<!-- Struttura Section -->
+<section id="struttura" class="content-section text-center">
+    <div class="struttura-section">
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>STRUTTURA</h2>
+                <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+                <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Locali Section -->
+<section id="locali" class="content-section text-center">
+    <div class="locali-section">
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>LOCALI</h2>
+                <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+                <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Dove Siamo Section -->
+<section id="dovesiamo" class="container content-section text-center">
+    <div class="dovesiamo-section">
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>DOVE SIAMO</h2>
+                <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+                <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Contatti Section -->
+<section id="contatti" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>CONTATTI</h2>
+            <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
+            <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+            </p>
+            <ul class="list-inline banner-social-buttons">
+                <li>
+                    <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                </li>
+                <li>
+                    <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                </li>
+                <li>
+                    <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php echo get_masterslider("1"); ?>
---
+++ wp-content/themes/casarabatti/old/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>&copy; Globe-it 2016</p>
+</footer>
+
+</div> <!-- /container -->
+<?php wp_footer(); ?>
+</body>
+</html>
