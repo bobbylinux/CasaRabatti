be31c2947fd2bc3a0cb41464d9e4e0f833579bba
bobbylinux <roberto.bani@gmail.com>
2016-03-10 20:57:27 +0100

    cambiata la slider in homepage

--- wp-content/themes/casarabatti/index.php
+++ wp-content/themes/casarabatti/index.php
@@ -1,12 +1,9 @@
 <?php get_header(); ?>
 <!-- qua andrà la slide -->
 <header>
-    <div class="header-content">
-        <div class="header-content-inner">
-            <?php masterslider(1); ?>
-            <h2 class="section-heading welcome-title"></h2>
-        </div>
-        </div>
+    <div class="header-content" id="slider">
+        <?php echo do_shortcode('[wonderplugin_slider id="2"]'); ?>
+    </div>
     </div>
 </header>
 
@@ -26,23 +23,23 @@
             <input type="text" class="form-control" id="partenza" placeholder="Partenza">
         </div>
         <a href="#" class="btn btn-default btn-xl">Verifica</a>
-    </div>   
+    </div>
 </form>
 
 <section class="bg-primary welcome" id="struttura">
-    <div class="container-fluid">    
-        <?php $the_post =  get_post(11); ?>
+    <div class="container-fluid">
+        <?php $the_post = get_post(11); ?>
         <div class="row">
             <div class="col-lg-12 text-center main-title">
-                <h2 class="section-heading"><?php echo  $the_post->post_title; ?></h2>
+                <h2 class="section-heading"><?php echo $the_post->post_title; ?></h2>
                 <img src="<?php echo get_template_directory_uri() . '/img/alloro.png'; ?>"
                      class="img-responsive primary" alt="">
             </div>
         </div>
-        <div class="row">           
+        <div class="row">
             <div class="col-lg-6 col-lg-offset-1 vcenter">
                 <p class="text-faded">
-                   <?php echo $the_post->post_content;?>
+                    <?php echo $the_post->post_content; ?>
                 </p>
             </div>
             <div class="col-lg-4 vcenter">
@@ -82,10 +79,10 @@
                 </div>
             </div>
             <div class="col-lg-6 vcenter">
-                <?php $the_post =  get_post(16); ?>
+                <?php $the_post = get_post(16); ?>
                 <div class="row">
                     <div class="col-lg-12 text-center">
-                        <h2 class="section-heading"><?php echo $the_post->post_title;?></h2>
+                        <h2 class="section-heading"><?php echo $the_post->post_title; ?></h2>
                         <img src="<?php echo get_template_directory_uri() . '/img/alloro.png'; ?>"
                              class="img-responsive primary" alt="">
                     </div>
@@ -93,7 +90,7 @@
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <p class="">
-                            <?php echo $the_post->post_content;?>
+                            <?php echo $the_post->post_content; ?>
                         </p>
                     </div>
                 </div>
@@ -105,10 +102,10 @@
 
 <section class="bg-primary welcome" id="dove-siamo">
     <div class="container-fluid">
-        <?php $the_post =  get_post(18); ?>
+        <?php $the_post = get_post(18); ?>
         <div class="row">
             <div class="col-lg-12 text-center main-title">
-                <h2 class="section-heading"><?php echo  $the_post->post_title; ?></h2>
+                <h2 class="section-heading"><?php echo $the_post->post_title; ?></h2>
                 <img src="<?php echo get_template_directory_uri() . '/img/alloro.png'; ?>"
                      class="img-responsive primary" alt="">
             </div>
@@ -116,7 +113,7 @@
         <div class="row">
             <div class="col-lg-5 col-lg-offset-1 vcenter">
                 <p class="text-faded">
-                    <?php echo $the_post->post_content;?>
+                    <?php echo $the_post->post_content; ?>
                 </p>
             </div>
             <div class="col-lg-5 vcenter">
@@ -129,37 +126,38 @@
 <section id="contatti" class="contacts">
     <div class="container">
 
-        <?php $the_post =  get_post(224); ?>
+        <?php $the_post = get_post(224); ?>
         <div class="row">
             <div class="col-lg-12 text-center main-title">
-                <h2 class="section-heading"><?php echo $the_post->post_title;?></h2>
+                <h2 class="section-heading"><?php echo $the_post->post_title; ?></h2>
                 <img src="<?php echo get_template_directory_uri() . '/img/alloro.png'; ?>"
                      class="img-responsive primary" alt="">
             </div>
         </div>
         <div class="row">
             <div class="col-lg-12 text-center">
-                <p><?php echo $the_post->post_content;?></p>
+                <p><?php echo $the_post->post_content; ?></p>
             </div>
         </div>
-            <div class="col-lg-4 col-lg-offset-2 text-center">
-                <i class="fa fa-phone fa-3x wow bounceIn"></i>
-                <p>+39 338.1534159</p>
-            </div>
-            <div class="col-lg-4 text-center">
-                <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+        <div class="col-lg-4 col-lg-offset-2 text-center">
+            <i class="fa fa-phone fa-3x wow bounceIn"></i>
 
-                <p><a href="mailto:info@casarabatti.it">info@casarabatti.it</a></p>
-            </div>
+            <p>+39 338.1534159</p>
         </div>
+        <div class="col-lg-4 text-center">
+            <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+
+            <p><a href="mailto:info@casarabatti.it">info@casarabatti.it</a></p>
+        </div>
+    </div>
     </div>
 </section>
 <section class="bg-primary partners" id="partners">
     <div class="container-fluid">
-        <?php $the_post =  get_post(227); ?>
+        <?php $the_post = get_post(227); ?>
         <div class="row">
             <div class="col-lg-12 text-center main-title">
-                <h2 class="section-heading"><?php echo  $the_post->post_title; ?></h2>
+                <h2 class="section-heading"><?php echo $the_post->post_title; ?></h2>
                 <img src="<?php echo get_template_directory_uri() . '/img/alloro.png'; ?>"
                      class="img-responsive primary" alt="">
             </div>
@@ -167,7 +165,7 @@
         <div class="row">
             <div class="col-lg-6 col-lg-offset-1 vcenter">
                 <p class="text-faded">
-                    <?php echo $the_post->post_content;?>
+                    <?php echo $the_post->post_content; ?>
                 </p>
             </div>
             <div class="col-lg-4 vcenter">
--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -1,7 +1,7 @@
 <?php
 // datastore=auditqueue;
 // created_on=1456137597;
-// updated_on=1457388863;
+// updated_on=1457638735;
 exit(0);
 ?>
 2bbb64d8a0a5a5aa163ccb9bb45622e6:{"created_at":1457167259,"message":null}
@@ -13,3 +13,7 @@ c620fc3fae3c0a872d6ecb799b25ed64:{"created_at":1457306629,"message":"Warning: 19
 d5bc8f0b02ba414dee8bd33071f67498:{"created_at":1457314800,"message":null}
 913ff9e28d072726e79490981a01634e:{"created_at":1457388627,"message":"Warning: 127.0.0.1; Post deleted; identifier: 84"}
 47547a08a49d9a6909b4b75bf635c936:{"created_at":1457388858,"message":null}
+54116b11f43323a5b33f82457377a02c:{"created_at":1457638086,"message":"Warning: admin, 192.168.1.2; Plugin activated: WonderPlugin Slider Free (v6.4; wonderplugin-slider\/wonderpluginslider.php)"}
+f28ef8289c126c47813b97b26af0ff43:{"created_at":1457638352,"message":"Warning: admin, 192.168.1.2; Plugin activated: WonderPlugin Slider Free (v6.4; wonderplugin-slider\/wonderpluginslider.php)"}
+70e9fe979e5258200f5e2b1fb9d46384:{"created_at":1457638540,"message":"Notice: admin, 192.168.1.2; Media file added; identifier: 230; name: sergio-appartamento-2-27-10-15-137-min; type: image\/jpeg"}
+e98a7e13872a24cd8c39b7406d694810:{"created_at":1457638730,"message":"Notice: admin, 192.168.1.2; Media file added; identifier: 232; name: sergio appartamento RRR 10-09-15 137-min; type: image\/jpeg"}
